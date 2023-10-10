9994ebdcb0f3a9654741cc4c6cb672d04fbd0faf
J3llyFi5h <lovely18012004@gmail.com>
2023-10-10 09:34:59 +0700

    home page and reward page

--- app/award/page.tsx
+++ app/award/page.tsx
@@ -1,56 +1,80 @@
 import Navbar from "../navbar/navbar";
-
+import Image from "next/image";
+import '../award/page.css'
 export default function award() {
   return (
     <main>
       <Navbar />
-      <section className=" bg-black h-[25vh] mt-20">
+      <section className=" h-auto mt-20">
         <div className="text-center">
-        <h1 className="text-4xl tracking-tight font-extrabold  sm:text-5xl md:text-6xl">Evaluation System</h1>
-        <p className="max-w-2xl mx-auto text-3xl mt-8">
-          Award of the year
-        </p>
+          <h1 className="text-4xl tracking-tight font-extrabold  sm:text-5xl md:text-6xl">
+            Evaluation System
+          </h1>
+          <p className="max-w-2xl mx-auto text-3xl mt-8">Award of the year</p>
         </div>
       </section>
 
-      <table className="table-auto mx-2">
-        <thead className="text-xs uppercase bg-blue-600">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              id
-            </th>
-            <th scope="col" className="px-6 py-3">
-              project name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              points
-            </th>
-            <th scope="col" className="px-6 py-3">
-              number
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="bg-blue-500 border-1 border-blue-400">
-            <td className="px-6 py-4">01</td>
-            <td className="px-6 py-4">Data</td>
-            <td className="px-6 py-4">122 point</td>
-            <td className="px-6 py-4">012</td>
-          </tr>
-          <tr className="bg-blue-600 border-b border-blue-400">
-            <td className="px-6 py-4">02</td>
-            <td className="px-6 py-4">The Eagles</td>
-            <td className="px-6 py-4">72 point</td>
-            <td className="px-6 py-4">112</td>
-          </tr>
-          <tr className="bg-blue-500 border-b border-blue-400">
-            <td className="px-6 py-4">03</td>
-            <td className="px-6 py-4">Tourism</td>
-            <td className="px-6 py-4">73 point</td>
-            <td className="px-6 py-4">102</td>
-          </tr>
-        </tbody>
-      </table>
+      <section className="h-auto">
+        <div className="flex flex-col justify-center items-center w-auto py-14">
+          <div className="relative w-auto">
+            <div className="mb-4 bg-gradient-to-r from-[#cc9910] to-[#fcf97c] shadow-2xl border-1 py-4 px-7 rounded-[2rem]">
+              <div className="flex gap-6  text-xl">
+                <p>1st</p>
+                
+                <div id="scroll-container">
+                <div id="scroll-text"> Evaluation System</div>
+  </div>
+
+                <p>100 point</p>
+                <p>003</p>
+              </div>
+            </div>
+            <Image
+              src="/1streward.png"
+              alt=""
+              width={35}
+              height={35}
+              className="absolute top-[-6px] left-[-10px] rotate-3"
+            />
+          </div>
+
+          <div className="relative w-auto">
+            <div className="mb-4 bg-gradient-to-r from-[#cc9910] to-[#fcf97c] shadow-2xl border-1 py-4 px-7 rounded-[2rem]">
+              <div className="flex gap-6  text-xl">
+                <p>1st</p>
+                <p>Data analysis</p>
+                <p>100 point</p>
+                <p>003</p>
+              </div>
+            </div>
+            <Image
+              src="/1streward.png"
+              alt=""
+              width={35}
+              height={35}
+              className="absolute top-[-6px] left-[-10px] rotate-3"
+            />
+          </div>
+
+          <div className="relative w-auto">
+            <div className="mb-4 bg-gradient-to-r from-[#cc9910] to-[#fcf97c] shadow-2xl border-1 py-4 px-7 rounded-[2rem]">
+              <div className="flex gap-6  text-xl">
+                <p>1st</p>
+                <p>Data analysis</p>
+                <p>100 point</p>
+                <p>003</p>
+              </div>
+            </div>
+            <Image
+              src="/1streward.png"
+              alt=""
+              width={35}
+              height={35}
+              className="absolute top-[-6px] left-[-10px] rotate-3"
+            />
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
