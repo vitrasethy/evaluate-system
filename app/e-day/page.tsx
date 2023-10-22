56eb55fb18daf9ae66ba1d200252f3060aa7dd66
vitrasethy <sethy.vitra@gmail.com>
2023-10-22 10:19:02 +0700

    responsive

--- app/e-day/page.tsx
+++ app/e-day/page.tsx
@@ -1,31 +1,8 @@
-"use client";
-import React from 'react';
-
-const Test = () => {
+export default function page() {
     return (
-        <div>
-            {isMobile() ? (
-                <h1>This is a mobile view</h1>
-            ) : (
-                <h1>This is a computer view</h1>
-            )}
-        </div>
-    );
-};
-
-const isMobile = () => {
-    const userAgent =
-        typeof window.navigator === 'undefined' ? '' : navigator.userAgent;
-    const mobile = Boolean(
-        userAgent.match(
-            /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
-        )
-    );
-    return mobile;
-};
-
-export default Test;
-
-
-
-
+      <div>
+          <div className='hidden lg:block'>This is Computer view</div>
+          <div className='lg:hidden'>This is Mobile view</div>
+      </div>
+    )
+  }
