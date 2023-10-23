b1b95fa87a047569a0f6a02476a8a604f5abba72
J3llyFi5h <lovely18012004@gmail.com>
2023-10-23 14:42:20 +0700

    update award

--- app/award/page.tsx
+++ app/award/page.tsx
@@ -21,16 +21,20 @@ export default function award() {
   }
   return (
   
-    <main>
-      <section className=" h-72 mt-20">
+    <main className="bg-[#191919]">
+      
+      <section className=" h-screen flex justify-center pt-[10%]">
         <div className="text-center">
-          <h1 className="text-4xl tracking-tight font-extrabold  sm:text-5xl md:text-6xl">
-            Evaluation System
+          <h1 className="text-4xl tracking-tight font-extrabold font-['Georgia'] uppercase sm:text-5xl md:text-8xl bg-gradient-to-r from-[#3b7cc1] to-[#9ae2ff] inline-block text-transparent bg-clip-text drop-shadow-[7px_5px_rgba(205,245,253,0.2)]">
+          Engineering's Day
           </h1>
-          <p className="max-w-2xl mx-auto text-3xl mt-8">Award of the year</p>
+          <p className="max-w-2xl mx-auto text-3xl mt-8 text-white">Award of Engineering's Day 2023</p>
+          <p className="max-w-2xl mx-auto text-3xl mt-8 text-white">Congratulation to the winners...</p>
         </div>
       </section>
-
+      <div className="animate-jump-in animate-infinite animate-duration-[5000ms] animate-ease-in text-red-600">
+        text
+      </div>
       <section className=" flex justify-center">
         <div className="flex flex-wrap justify-center gap-8 p-8 items-center border-2 bg-slate-50 w-[70%]">
 
@@ -39,7 +43,7 @@ export default function award() {
               <div className="flex gap-8 text-3xl">
                 <p>1st</p>
                 <TextWithAnimation text={'GASTROMOD: AN INTERACTIVE TOOL FOR 3D GASTROPOD MODELING'}/>
-                <p>100 point</p>
+                <p>100&nbsp;point</p>
                 <p>003</p>
               </div>
             </div>
@@ -53,11 +57,11 @@ export default function award() {
           </div>
 
           <div className="relative">
-          <div className="bg-gradient-to-r from-[#cc9910] to-[#fcf97c] shadow-2xl border-1 py-6 px-10 rounded-full">
+          <div className="bg-gradient-to-r from-[#9694A0] to-[#E3E1EB] shadow-2xl border-1 py-6 px-10 rounded-full">
               <div className="flex gap-8 text-3xl">
                 <p>2nd</p>
                 <TextWithAnimation text={'ASEAN FACTORI 4.0 PROJECT'}/>
-                <p>100 point</p>
+                <p>100&nbsp;point</p>
                 <p>003</p>
               </div>
             </div>
@@ -71,16 +75,16 @@ export default function award() {
           </div>
 
           <div className="relative">
-            <div className="bg-gradient-to-r from-[#cc9910] to-[#fcf97c] shadow-2xl border-1 py-6 px-10 rounded-full">
+            <div className="bg-gradient-to-r from-[#D16A38] to-[#F0C9BA] shadow-2xl border-1 py-6 px-10 rounded-full">
               <div className="flex gap-8 text-3xl">
                 <p>2nd</p>
                 <TextWithAnimation text={'A NEW MODEL FOR SIMULATING AND EVALUATING CONGESTION CAUSE AT SIGNALIZED INTERSECTION'}/>
-                <p>100 point</p>
+                <p>100&nbsp;point</p>
                 <p>003</p>
               </div>
             </div>
             <Image
-              src="/1st.png"
+              src="/3rd.png"
               alt=""
               width={50}
               height={50}
